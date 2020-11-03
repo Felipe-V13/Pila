6ee518596165883738a9e297f1003af93cef991b
Felipe_Vargas13 <pipevarji@gmail.copm>
2020-11-03 02:14:05 -0600

    new

--- src/Main.java
+++ src/Main.java
@@ -18,30 +18,30 @@ public class Main {
         System.out.println("\n<<-- Tamaño -->");
         System.out.println(pila.getTamanio());
 
-        System.out.println("\n<<-- Retirar el elemento del tope de la pila -->>");
+        /*System.out.println("\n<<-- Retirar el elemento del tope de la pila -->>");
         pila.retirar();
         pila.listar();
-        System.out.println("Tamaño: " + pila.getTamanio());
+        System.out.println("Tamaño: " + pila.getTamanio());*/
 
-        System.out.println("\n<<-- Actualizar el valor del nodo con el valor 12 por 24 -->>");
+        /*System.out.println("\n<<-- Actualizar el valor del nodo con el valor 12 por 24 -->>");
         pila.editar(12, 44);
         pila.listar();
-        System.out.println("Tamaño: " + pila.getTamanio());
+        System.out.println("Tamaño: " + pila.getTamanio());*/
 
-        System.out.println("\n<<-- Eliminar el nodo con el valor 16 -->>");
+        /*System.out.println("\n<<-- Eliminar el nodo con el valor 16 -->>");
         pila.remover(16);
         pila.listar();
-        System.out.println("Tamaño: " + pila.getTamanio());
+        System.out.println("Tamaño: " + pila.getTamanio());*/
 
-        System.out.println("\n<<-- Consulta si existe el valor 65 -->>");
-        System.out.println(pila.buscar(65));
+        /*System.out.println("\n<<-- Consulta si existe el valor 65 -->>");
+        System.out.println(pila.buscar(65));*/
 
-        System.out.println("\n<<-- Elimina la pila -->>");
-        pila.eliminar();
+        /*System.out.println("\n<<-- Elimina la pila -->>");
+        pila.eliminar();*/
 
         System.out.println("\n<<-- Consulta si la pila esta vacia -->>");
         System.out.println(pila.esVacia());
 
-        System.out.println("\n\n<<-- Fin de ejemplo pila -->>");
+        /*System.out.println("\n\n<<-- Fin de ejemplo pila -->>");*/
     }
 }
